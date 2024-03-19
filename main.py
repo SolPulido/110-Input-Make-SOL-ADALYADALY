de62f1b5d559f447407942d508a76ef2afa89bbd
SolPulido <144694649+SolPulido@users.noreply.github.com>
2024-03-19 22:26:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#Sol pulido
+#09/18/23
+print("What is your name?") 
+userName=input("")
+
+print("Nice to meet you ! "  +  userName  +  "  Welcome to my page ")
+
+print(" What's your favorite movie? \n What type of music you like?\n What's your favorite color?")
+
+movie = input(" ")
+music = input(" ")
+color = input(" ")
+print("Your favorite movie is " + movie + " , your favorite music is " + music + " , your favorite color is " + color)
+
+
